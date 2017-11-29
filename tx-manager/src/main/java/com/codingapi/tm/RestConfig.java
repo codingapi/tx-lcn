3636d4f328aa353e97b07d6bc6272f290d19faa3
wangliang <1991wangliang@gmail.com>
2017-11-29 15:49:00 +0800

    移除不用的jar

--- tx-manager/src/main/java/com/codingapi/tm/RestConfig.java
+++ tx-manager/src/main/java/com/codingapi/tm/RestConfig.java
@@ -1,8 +1,6 @@
 package com.codingapi.tm;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
  */
 
 @Configuration
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableAutoConfiguration
 public class RestConfig {
 
 
