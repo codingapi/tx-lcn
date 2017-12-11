a8dbdb21026d972b2138997affc4850fcb481580
foxshooting <fox_wudi@126.com>
2017-12-11 22:36:47 +0800

    coding style aligned with master

--- transaction-springcloud/src/main/java/com/codingapi/ribbon/loadbalancer/LcnLoadBalancerRule.java
+++ transaction-springcloud/src/main/java/com/codingapi/ribbon/loadbalancer/LcnLoadBalancerRule.java
@@ -1,5 +1,6 @@
 package com.codingapi.ribbon.loadbalancer;
 
+import com.alibaba.fastjson.JSONObject;
 import com.codingapi.tx.aop.bean.TxTransactionLocal;
 import com.lorne.core.framework.utils.encode.MD5Util;
 import com.netflix.loadbalancer.Server;
@@ -16,42 +17,46 @@ public class LcnLoadBalancerRule {
 	
 	private Logger logger = LoggerFactory.getLogger(LcnLoadBalancerRule.class);
 	
-	public Server proxy(List<Server> qualifiedServers, Server server){
-		logger.info("The selected server info, host:" + server.getHost() + ", port:" + server.getPort());
+	public Server proxy(List<Server> servers,Server server){
+
 		TxTransactionLocal txTransactionLocal = TxTransactionLocal.current();
-		if(txTransactionLocal == null){
+		if(txTransactionLocal==null){
 			return server;
 		}
-		
-		String groupId = txTransactionLocal.getGroupId();
 
-		//取出组件的appName
-		String appName = server.getMetaInfo().getAppName();
+		try{
+			logger.info("LCNBalanceProxy - > start");
 
-		logger.info("The model named " + appName + " is going to be called, groupId:" + groupId);
+			String groupId = txTransactionLocal.getGroupId();
 
-		String key = MD5Util.md5((groupId + "_" + appName).getBytes());
-		
-		Server cachedServer = getInvoker(txTransactionLocal, qualifiedServers, key);
+			//取出组件的appName
+			String appName = server.getMetaInfo().getAppName();
 
-		if(cachedServer == null){
-			logger.info("The server of key:" + key + " has not been cached yet!");
-			putInvoker(key, txTransactionLocal, server);
+
+			String key = MD5Util.md5((groupId + "_" + appName).getBytes());
+
+			Server oldServer =getServer(txTransactionLocal,servers,key);
+			if(oldServer != null){
+				logger.info("LCNBalanceProxy - > load old server ");
+				return server;
+			}
+
+			putServer(key, txTransactionLocal, server);
+			logger.info("LCNBalanceProxy - > load new server ");
+
 			return server;
-		} else{
-			logger.info("The cached server info, host:" + cachedServer.getHost() + ", port:" + cachedServer.getPort());
-			return cachedServer;
+		}finally {
+			logger.info("LCNBalanceProxy - > end");
 		}
-		
 	}
 
 
-	private void putInvoker(String key,TxTransactionLocal txTransactionLocal,Server server){
-		String appName = server.getMetaInfo().getAppName();
 
-		String address = server.getHost() + ":" + server.getPort();
+	private void putServer(String key,TxTransactionLocal txTransactionLocal,Server server){
+		String serviceName =  server.getMetaInfo().getAppName();
+		String address = server.getHostPort();
 
-		String md5 = MD5Util.md5((address+appName).getBytes());
+		String md5 = MD5Util.md5((address+serviceName).getBytes());
 
 		logger.info("putServer->address->"+address+",md5-->"+md5);
 
@@ -59,17 +64,16 @@ public class LcnLoadBalancerRule {
 	}
 
 
-	private Server getInvoker(TxTransactionLocal txTransactionLocal, List<Server> servers,String key){
+	private Server getServer(TxTransactionLocal txTransactionLocal, List<Server> servers, String key){
 		String val = txTransactionLocal.getLoadBalance(key);
 		if(StringUtils.isEmpty(val)){
 			return null;
 		}
 		for(Server server:servers){
-			String appName =  server.getMetaInfo().getAppName();
-			//格式统一为host:port
-			String address = server.getHost() + ":" + server.getPort();
+			String serviceName =  server.getMetaInfo().getAppName();
+			String address = server.getHostPort();
 
-			String md5 = MD5Util.md5((address+appName).getBytes());
+			String md5 = MD5Util.md5((address+serviceName).getBytes());
 
 			logger.info("getServer->address->"+address+",md5-->"+md5);
 
