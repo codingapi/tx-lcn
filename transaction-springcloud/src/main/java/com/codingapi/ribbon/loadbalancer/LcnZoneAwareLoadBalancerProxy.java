e174acb880e4e177b67c87739522fc3d6d718914
foxshooting <fox_wudi@126.com>
2018-01-16 21:20:32 +0800

    运行时能更新服务清单

--- transaction-springcloud/src/main/java/com/codingapi/ribbon/loadbalancer/LcnZoneAwareLoadBalancerProxy.java
+++ transaction-springcloud/src/main/java/com/codingapi/ribbon/loadbalancer/LcnZoneAwareLoadBalancerProxy.java
@@ -5,6 +5,7 @@ import com.netflix.loadbalancer.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,7 +26,12 @@ public class LcnZoneAwareLoadBalancerProxy extends ZoneAwareLoadBalancer<Server>
 	@Override
 	public Server chooseServer(Object key){
 		logger.info("enter chooseServer method, key:" + key);
-        List<Server> serverList = super.getReachableServers();
+		//利用Dynamic负载类中的方法，可以在运行时更新服务实例清单
+		List<Server> serverList = new ArrayList<Server>();
+		//获取处理之后的serverlist
+		serverList = super.getServerListImpl().getUpdatedListOfServers();
+		//获取过滤之后的serverlist
+		serverList = super.getFilter().getFilteredListOfServers(serverList);
         if(null == serverList || serverList.isEmpty()){
 		    return super.chooseServer(key);
         }
