2ef085e53dbab5215ea98db36f1cfebdc12e3889
wangliang <1991wangliang@gmail.com>
2018-01-22 10:48:49 +0800

    UpdatedListOfServers

--- transaction-springcloud/src/main/java/com/codingapi/ribbon/loadbalancer/LcnZoneAwareLoadBalancerProxy.java
+++ transaction-springcloud/src/main/java/com/codingapi/ribbon/loadbalancer/LcnZoneAwareLoadBalancerProxy.java
@@ -5,6 +5,7 @@ import com.netflix.loadbalancer.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,20 +16,25 @@ public class LcnZoneAwareLoadBalancerProxy extends ZoneAwareLoadBalancer<Server>
 	private Logger logger = LoggerFactory.getLogger(LcnZoneAwareLoadBalancerProxy.class);
 	
 	LcnLoadBalancerRule lcnLoadBalancerRule = new LcnLoadBalancerRule();
-	
+
 	public LcnZoneAwareLoadBalancerProxy(IClientConfig clientConfig, IRule rule,
-            IPing ping, ServerList<Server> serverList, ServerListFilter<Server> filter,
-            ServerListUpdater serverListUpdater) {
+										 IPing ping, ServerList<Server> serverList, ServerListFilter<Server> filter,
+										 ServerListUpdater serverListUpdater) {
 		super(clientConfig, rule, ping, serverList, filter, serverListUpdater);
 	}
 
 	@Override
 	public Server chooseServer(Object key){
-		logger.debug("enter chooseServer method, key:" + key);
-        List<Server> serverList = super.getReachableServers();
-        if(null == serverList || serverList.isEmpty()){
-		    return super.chooseServer(key);
-        }
+		logger.info("enter chooseServer method, key:" + key);
+		//利用Dynamic负载类中的方法，可以在运行时更新服务实例清单
+		List<Server> serverList = new ArrayList<Server>();
+		//获取处理之后的serverlist
+		serverList = super.getServerListImpl().getUpdatedListOfServers();
+		//获取过滤之后的serverlist
+		serverList = super.getFilter().getFilteredListOfServers(serverList);
+		if(null == serverList || serverList.isEmpty()){
+			return super.chooseServer(key);
+		}
 		return lcnLoadBalancerRule.proxy(serverList, super.chooseServer(key));
 
 	}
