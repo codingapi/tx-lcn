a129e03d959021be91c4aef72650da92e0eeff2b
Tneciv <tnecivl@gmail.com>
2018-08-31 11:39:18 +0800

    upgrade libs

--- tx-manager/src/main/java/com/codingapi/tm/TxManagerApplication.java
+++ tx-manager/src/main/java/com/codingapi/tm/TxManagerApplication.java
@@ -2,11 +2,11 @@ package com.codingapi.tm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class TxManagerApplication {
 
 
