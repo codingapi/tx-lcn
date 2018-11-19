9b47f36e24f22f5d334bc9c474d9081c031a5550
807924995@qq.com <qq807924995>
2018-11-19 19:36:55 +0800

    兼容 2.0方案 启动监听容器刷新事件 备注:如果用外置tomcat启动,而且配置的端口和外置端口不一致可能会导致读取端口错误

--- transaction-springcloud/src/main/java/com/codingapi/tx/springcloud/listener/ServerListener.java
+++ transaction-springcloud/src/main/java/com/codingapi/tx/springcloud/listener/ServerListener.java
@@ -4,25 +4,36 @@ import com.codingapi.tx.listener.service.InitService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 
 @Component
-public class ServerListener implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
+public class ServerListener implements  ApplicationListener<ApplicationStartedEvent> {
 
     private Logger logger = LoggerFactory.getLogger(ServerListener.class);
 
+    @Value("${server.port}")
     private int serverPort;
 
     @Autowired
     private InitService initService;
 
+
+    public int getPort() {
+        return this.serverPort;
+    }
+    
+    public void setServerPort(int serverPort) {
+        this.serverPort = serverPort;
+    }
+
     @Override
-    public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
-        logger.info("onApplicationEvent -> onApplicationEvent. "+event.getEmbeddedServletContainer());
-        this.serverPort = event.getEmbeddedServletContainer().getPort();
+    public void onApplicationEvent(ApplicationStartedEvent event) {
+        logger.info("onApplicationEvent -> onApplicationEvent. "+event.getClass());
 
         Thread thread = new Thread(new Runnable() {
             @Override
@@ -33,13 +44,5 @@ public class ServerListener implements ApplicationListener<EmbeddedServletContai
         });
         thread.setName("TxInit-thread");
         thread.start();
-    }
-
-    public int getPort() {
-        return this.serverPort;
-    }
-    
-    public void setServerPort(int serverPort) {
-        this.serverPort = serverPort;
     }
 }
