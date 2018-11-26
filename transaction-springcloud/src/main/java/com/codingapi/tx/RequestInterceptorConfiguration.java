bc2606742a6bd87f671dbf7692d0991ec019f25a
何鹰 <yinghe2@iflytek.com>
2018-11-26 17:56:33 +0800

    添加RestTemplate 拦截器设置

--- transaction-springcloud/src/main/java/com/codingapi/tx/RequestInterceptorConfiguration.java
+++ transaction-springcloud/src/main/java/com/codingapi/tx/RequestInterceptorConfiguration.java
@@ -1,9 +1,12 @@
 package com.codingapi.tx;
 
 import com.codingapi.tx.springcloud.feign.TransactionRestTemplateInterceptor;
+import com.codingapi.tx.springcloud.http.TransactionHttpRequestInterceptor;
 import feign.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * create by lorne on 2018/1/18
@@ -14,5 +17,14 @@ public class RequestInterceptorConfiguration {
     @Bean
     public RequestInterceptor requestInterceptor(){
         return new TransactionRestTemplateInterceptor();
+    }
+
+    @Bean
+    public TransactionHttpRequestInterceptor transactionHttpRequestInterceptor(@Autowired(required = false) RestTemplate restTemplate){
+        TransactionHttpRequestInterceptor transactionHttpRequestInterceptor = new TransactionHttpRequestInterceptor();
+        if(restTemplate != null){
+            restTemplate.getInterceptors().add(transactionHttpRequestInterceptor);
+        }
+        return transactionHttpRequestInterceptor;
     }
 }
