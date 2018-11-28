48c605f0716d3875050b2effbf82fc7b9ee3e7c4
yizhishang <285206405@qq.com>
2018-11-28 18:28:25 +0800

    删除多余的文件：dubbo和motan

--- tx-manager/src/main/java/com/codingapi/tm/TxManagerApplication.java
+++ tx-manager/src/main/java/com/codingapi/tm/TxManagerApplication.java
@@ -9,8 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class TxManagerApplication {
 
-
-
     public static void main(String[] args) {
         SpringApplication.run(TxManagerApplication.class, args);
     }
