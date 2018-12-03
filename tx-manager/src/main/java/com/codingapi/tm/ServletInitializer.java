caecf11f02542682e8703b60395ea386ba08291c
yizhishang <285206405@qq.com>
2018-12-03 20:32:40 +0800

    format

--- tx-manager/src/main/java/com/codingapi/tm/ServletInitializer.java
+++ tx-manager/src/main/java/com/codingapi/tm/ServletInitializer.java
@@ -8,11 +8,9 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  */
 public class ServletInitializer extends SpringBootServletInitializer {
 
-
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(TxManagerApplication.class);
     }
-
 
 }
