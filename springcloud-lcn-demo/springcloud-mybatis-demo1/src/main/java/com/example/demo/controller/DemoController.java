62f653238634a68c7885f4585014821a664dcddb
yizhishang <285206405@qq.com>
2018-12-05 16:56:58 +0800

    notifyResult请求方式修改成post

--- springcloud-lcn-demo/springcloud-mybatis-demo1/src/main/java/com/example/demo/controller/DemoController.java
+++ springcloud-lcn-demo/springcloud-mybatis-demo1/src/main/java/com/example/demo/controller/DemoController.java
@@ -6,10 +6,7 @@ import com.codingapi.tx.compensate.model.CompensateInfo;
 import com.example.demo.entity.Test;
 import com.example.demo.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,7 +34,7 @@ public class DemoController {
         return demoService.save(id, name);
     }
 
-    @GetMapping("notifyResult")
+    @PostMapping("notifyResult")
     public String notifyResult(String jsonStr)
     {
         System.out.println("通知地址...");
