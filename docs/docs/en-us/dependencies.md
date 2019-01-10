d00d25e85a5f29732214ad2ff906f982009c4dca
ujued <ujued@qq.com>
2019-01-10 09:56:37 +0800

    docs

---
+++ docs/docs/en-us/dependencies.md
@@ -0,0 +1,29 @@
+# 依赖
+
+## 基础依赖
+* JDK 8+
+* Maven 3（可选的）
+* SpringBoot 2.x
+
+## Java库依赖
+* projectlombok:1.18.0
+* hessian:4.0.38
+* fastjson:1.2.34
+* protostuff1.1.3
+* kryo:4.0.0
+* brave-instrumentation:5.4.3
+* commons-dbutils:1.7
+* jsqlparser:1.3
+* h2database:1.4.197
+* hikari-cp:3.1.0
+* pagehelper:1.2.10 `TxManager`
+* mybatis.spring.boot:1.3.2 `TxManager`
+* spring-cloud:Finchley.SR2 (可选的)
+* netty:4.1.31.Final （可选的）
+* dubbo:0.2.0 (可选的)
+
+
+## 中间件依赖
+* Mariadb 10+ (MySQL5+)
+* Redis any
+* 微服务注册中心（zookeeper, consul） (可选的)
--- docs/site_config/docs.js
+++ docs/site_config/docs.js
@@ -2,30 +2,126 @@ export default {
     'en-us': {
         sidemenu: [
             {
-                title: 'header title',
+                title: '用户文档',
                 children: [
                     {
-                        title: 'demo1',
-                        link: '/en-us/docs/demo1.html'
-                    },
-                    {
-                        title: 'demo2',
-                        link: '/en-us/docs/demo2.html'
-                    },
-                    {
-                        title: 'dir',
+                        title: '入门',
+                        link: '/en-us/docs/preface.html',
                         opened: true,
+                    },
+                    {
+                        title: '背景',
+                        link: '/en-us/docs/background.html',
+                        opened: true,
+                    },
+                    {
+                        title: '快速开始',
+                        link: '/en-us/docs/start.html',
+                    },
+                    {
+                        title: '依赖',
+                        link: '/en-us/docs/dependencies.html',
+                    },
+                    {
+                        title: '示例',
                         children: [
                             {
-                                title: 'demo3',
-                                link: '/en-us/docs/dir/demo3.html',
-                            }
+                                title: '基础环境',
+                                link: '/en-us/docs/demo/env.html',
+                            },
+                            {
+                                title: 'dubbo示例',
+                                link: '/en-us/docs/demo/dubbo.html',
+                            },
+                            {
+                                title: 'springcloud示例',
+                                link: '/en-us/docs/demo/springcloud.html',
+                            },
                         ],
-                    }
-                ]
-            }
+                    },
+                    {
+                        title: '原理介绍',
+                        children: [
+                            {
+                                title: '控制原理',
+                                link: '/en-us/docs/principle/control.html',
+                            },
+                            {
+                                title: 'LCN模式',
+                                link: '/en-us/docs/principle/lcn.html',
+                            },
+                            {
+                                title: 'TCC模式',
+                                link: '/en-us/docs/principle/tcc.html',
+                            },
+                            {
+                                title: 'TXC模式',
+                                link: '/en-us/docs/principle/txc.html',
+                            },
+                        ],
+                    },
+                    {
+                        title: '配置手册',
+                        children: [
+                            {
+                                title: 'TxClient配置',
+                                link: '/en-us/docs/setting/client.html',
+                            },
+                            {
+                                title: 'TxManager配置',
+                                link: '/en-us/docs/setting/manager.html',
+                            },
+                            {
+                                title: '集群与负载',
+                                link: '/en-us/docs/setting/distributed.html',
+                            },
+                        ],
+                    },
+                    {
+                        title: '扩展支持',
+                        children: [
+                            {
+                                title: '事务模式扩展',
+                                link: '/en-us/docs/expansion/transaction.html',
+                            },
+                            {
+                                title: '通讯协议扩展',
+                                link: '/en-us/docs/expansion/message.html',
+                            },
+                            {
+                                title: 'RPC框架扩展',
+                                link: '/en-us/docs/expansion/rpc.html',
+                            },
+                        ],
+                    },
+                    {
+                        title: '通讯指令手册',
+                        link: '/en-us/docs/communication.html',
+                    },
+                    {
+                        title: 'TM管理手册',
+                        link: '/en-us/docs/manageradmin.html',
+                    },
+                    {
+                        title: '问题排查手册',
+                        link: '/en-us/docs/debug.html',
+                    },
+                    // {
+                    //     title: '性能测试报告',
+                    //     link: '/zh-cn/docs/test.html',
+                    // },
+                    {
+                        title: '开发者',
+                        link: '/en-us/docs/developer.html',
+                    },
+                    {
+                        title: 'FQA',
+                        link: '/en-us/docs/fqa.html',
+                    },
+                ],
+            },
         ],
-        barText: 'Documentation',
+        barText: '文档',
     },
     'zh-cn': {
         sidemenu: [
@@ -134,10 +230,10 @@ export default {
                         title: '问题排查手册',
                         link: '/zh-cn/docs/debug.html',
                     },
-                    {
-                        title: '性能测试报告',
-                        link: '/zh-cn/docs/test.html',
-                    },
+                    // {
+                    //     title: '性能测试报告',
+                    //     link: '/zh-cn/docs/test.html',
+                    // },
                     {
                         title: '开发者',
                         link: '/zh-cn/docs/developer.html',
