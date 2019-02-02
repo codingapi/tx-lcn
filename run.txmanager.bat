9cc577a498010b12641f3f2626421be161378255
wangliang <1991wangliang@gmail.com>
2019-02-02 10:39:08 +0800

    5.0.0

--- run.txmanager.bat
+++ run.txmanager.bat
@@ -1,4 +1,4 @@
 @echo off
 
-start java -jar  -Xms256m -Xmx512m  txlcn-tm/target/txlcn-tm-5.0.0.RC2.jar
+start java -jar  -Xms256m -Xmx512m  txlcn-tm/target/txlcn-tm-5.0.0.jar
 
--- txlcn-tm/src/main/java/com/codingapi/txlcn/tm/support/TxLcnManagerBanner.java
+++ txlcn-tm/src/main/java/com/codingapi/txlcn/tm/support/TxLcnManagerBanner.java
@@ -32,7 +32,7 @@ import java.io.PrintStream;
  */
 public class TxLcnManagerBanner implements Banner {
 
-    public static final String VERSION = "5.0.0.RC2";
+    public static final String VERSION = "5.0.0";
 
     private static final String BANNER =
             "          _______   __      _     _____  _   _          \n" +
