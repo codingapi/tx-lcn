5a22bb79abc7335b3384b02883ee5db5fb3e0249
ujued <ujued@qq.com>
2019-02-20 16:18:30 +0800

    ribbon

--- txlcn-tracing/src/main/java/com/codingapi/txlcn/tracing/http/ribbon/LoadbalancerConfiguration.java
+++ txlcn-tracing/src/main/java/com/codingapi/txlcn/tracing/http/ribbon/LoadbalancerConfiguration.java
@@ -34,7 +34,7 @@ import org.springframework.context.annotation.Scope;
 public class LoadbalancerConfiguration {
 
     @Bean
-    @ConditionalOnProperty(name = "tx-lcn.ribbon.loadbalancer.dtx.enabled", havingValue = "true")
+    @ConditionalOnProperty(name = "tx-lcn.ribbon.loadbalancer.dtx.enabled", matchIfMissing = true)
     @Scope("prototype")
     public IRule ribbonRule(Registration registration) {
         return new TxlcnZoneAvoidanceRule(registration);
