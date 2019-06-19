8a373bf90eb73914b9c2d94e48b8dc085ad70705
yizhishang <285206405@qq.com>
2019-06-19 14:00:24 +0800

    tx-manager后台展示client模块列表添加ip区分相同应用

---
+++ txlcn-tm/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker run -e PARAMS="--spring.redis.host=192.168.110.119 --spring.redis.database=0 --tx-lcn.manager.host=192.168.110.119" \
+ -p 7970:7970 -p 8070:8070 \
+ --name=tx-manager 119.yizhishang.com:5000/yizhishang/tx-manager:latest
