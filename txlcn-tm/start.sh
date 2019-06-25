cb0a3d69b1a7bdfc1beff130703708affa60029e
yizhishang <285206405@qq.com>
2019-06-25 10:55:18 +0800

    优化部署文件

--- txlcn-tm/start.sh
+++ txlcn-tm/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+docker stop tx-manager
+docker rm tx-manager
+
 docker run -e PARAMS="--spring.redis.host=192.168.110.119 --spring.redis.database=0 --tx-lcn.manager.host=192.168.110.119" \
  -p 7970:7970 -p 8070:8070 \
  --name=tx-manager 119.yizhishang.com:5000/yizhishang/tx-manager:latest
