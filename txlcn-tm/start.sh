7e74813b0f7e74fb58eecb2992d4f8d27dc50c33
yizhishang <285206405@qq.com>
2019-06-26 17:24:33 +0800

    修改启动脚本

--- txlcn-tm/start.sh
+++ txlcn-tm/start.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+echo 'docker stop tx-manager...'
 docker stop tx-manager
+
+echo 'docker rm tx-manager...'
 docker rm tx-manager
 
-docker run -e PARAMS="--spring.redis.host=192.168.110.119 --spring.redis.database=0 --tx-lcn.manager.host=192.168.110.119" \
- -p 7970:7970 -p 8070:8070 \
+docker run -p 7970:7970 -p 8070:8070 \
+ -e "server.port=7970" \
+ -e "spring.redis.host=192.168.110.119" \
+ -e "spring.datasource.url=jdbc:mysql://192.168.110.1:3306/tx-manager?characterEncoding=UTF-8&serverTimezone=UTC&useSSL=false" \
+ -e "spring.datasource.username=root" \
+ -e "spring.datasource.password=root" \
+ -e "tx-lcn.manager.host=192.168.110.119" \
  --name=tx-manager 119.yizhishang.com:5000/yizhishang/tx-manager:latest
