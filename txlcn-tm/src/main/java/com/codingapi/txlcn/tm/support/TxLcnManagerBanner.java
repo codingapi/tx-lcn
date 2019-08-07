b70544f39fa84e635e2185ac9eeb16cfa1995c86
yizhishang <285206405@qq.com>
2019-08-07 09:50:34 +0800

    整理代码

--- txlcn-tm/src/main/java/com/codingapi/txlcn/tm/support/TxLcnManagerBanner.java
+++ txlcn-tm/src/main/java/com/codingapi/txlcn/tm/support/TxLcnManagerBanner.java
@@ -32,7 +32,7 @@ import java.io.PrintStream;
  */
 public class TxLcnManagerBanner implements Banner {
 
-    public static final String VERSION = "1.0.0.RELEASE";
+    public static final String VERSION = "5.0.2.RELEASE";
 
     private static final String BANNER =
             "          _______   __      _     _____  _   _          \n" +
