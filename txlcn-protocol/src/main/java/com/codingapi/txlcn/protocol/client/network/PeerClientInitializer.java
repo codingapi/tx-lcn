c7cff413f69073c571ca767df09ccd0209bcaab1
wangliang <1991wangliang@gmail.com>
2020-03-03 15:40:28 +0800

    codecov commit.

--- txlcn-protocol/src/main/java/com/codingapi/txlcn/protocol/client/network/PeerClientInitializer.java
+++ txlcn-protocol/src/main/java/com/codingapi/txlcn/protocol/client/network/PeerClientInitializer.java
@@ -5,6 +5,8 @@ import com.codingapi.txlcn.protocol.Config;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
 import io.netty.handler.codec.serialization.ClassResolvers;
 import io.netty.handler.codec.serialization.ObjectDecoder;
 import io.netty.handler.codec.serialization.ObjectEncoder;
@@ -32,11 +34,16 @@ public class PeerClientInitializer extends ChannelInitializer<SocketChannel> {
 
   @Override
   protected void initChannel(final SocketChannel ch) throws Exception {
-    final ChannelPipeline pipeline = ch.pipeline();
+    ChannelPipeline pipeline = ch.pipeline();
+
+    pipeline.addLast(new LengthFieldPrepender(4, false));
+    pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
+
     pipeline.addLast(new ObjectDecoder(ClassResolvers.cacheDisabled(null)));
     pipeline.addLast(encoder);
     pipeline.addLast(new IdleStateHandler(config.getMaxReadIdleSeconds(),
         config.getMaxWriteIdleSeconds(), 0));
+
     pipeline.addLast(peerChannelHandlerExecutorGroup, peerClientHandler);
   }
 
