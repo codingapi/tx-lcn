8de99c94c37876acb459dbcf08d7ec36f258598d
wangliang <1991wangliang@gmail.com>
2020-07-02 18:59:53 +0800

    configuration

--- txlcn-tc/src/main/java/com/codingapi/txlcn/tc/jdbc/JdbcConfiguration.java
+++ txlcn-tc/src/main/java/com/codingapi/txlcn/tc/jdbc/JdbcConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
 import java.util.List;
 
 /**
