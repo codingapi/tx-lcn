311e1ecae7eb9c98d270ee4e6eeb25306b978443
boylong12 <boylong08@126.com>
2020-10-28 16:20:12 +0800

    dubbo provider interceptor
    Change-Id: I588c300bd54baf7db308e782daf9f2828cfce29e

--- txlcn-tc/src/main/java/com/codingapi/txlcn/tc/rpc/DubboConsumerRpcTransactionInterceptor.java
+++ txlcn-tc/src/main/java/com/codingapi/txlcn/tc/rpc/DubboConsumerRpcTransactionInterceptor.java
@@ -9,7 +9,7 @@ import com.alibaba.dubbo.rpc.*;
  * @date 2020/10/28 11:01
  */
 @Activate(group = Constants.CONSUMER)
-public class DubboRpcTransactionInterceptor implements Filter {
+public class DubboConsumerRpcTransactionInterceptor implements Filter {
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         RpcTransactionContext.getInstance().build(invocation.getAttachments()::put);
